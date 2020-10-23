ea8d58fd1a97c59a53f65e3a1c0223c247e1e49a
vinay-khandwe <vinay1998khandwe@gmail.com>
2020-10-23 12:27:30 +0530

    feature 1 changes

--- src/main/java/com/example/demo/TestProjectApplication.java
+++ src/main/java/com/example/demo/TestProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		System.out.println("Test feature");
 		SpringApplication.run(TestProjectApplication.class, args);
 	}
 
